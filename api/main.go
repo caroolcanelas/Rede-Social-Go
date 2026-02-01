38759c608718b72bd37413f42cf0d286cf287711
caroolcanelas <carolina.canelas@soulasalle.com.br>
2026-02-01 12:52:44 -0300

    Conexao com o banco

--- api/main.go
+++ api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"api/src/config"
 	"api/src/router"
 	"fmt"
 	"log"
@@ -8,9 +9,10 @@ import (
 )
 
 func main(){
-	fmt.Println("rodando API")
-
+	config.Carregar()
 	r := router.Gerar()
 
-	log.Fatal(http.ListenAndServe(":5001", r))
+	fmt.Println("Escutando na porta %d", config.Porta)
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 }
